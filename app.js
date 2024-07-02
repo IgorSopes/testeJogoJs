79d0bc1e517c3695936dec151ce841ad3acd6b2c
Igor Sopes <sopesigor@gmail.com>
2024-07-02 20:40:14 -0300

    incluindo logica no arquivo

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 alert('Bem vindo ao jogo do número secreto!');
+let numeroSecreto = 4;
+console.log(numeroSecreto)
+let chute = prompt('Escolha um número entre 1 e 10');
+
+if (chute == numeroSecreto) {
+    alert('Parabéns, você acertou o número secreto!');
+} else {
+    alert ('Você errou! Tente de novo')
+
+}
