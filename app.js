822fe6b2e96b5646e2b5182c76540338d90c009b
Igor Sopes <sopesigor@gmail.com>
2024-07-02 20:15:54 -0300

    novo commit

--- app.js
+++ app.js
@@ -1,9 +1,1 @@
-let mensagemDeErro = 'Erro! Preencha todos os campos!'
-alert(mensagemDeErro);
-
-
-
-var number1 = 2
-var number2 = 4
-console.log(number1 + number2);
-
+alert('Bem vindo ao jogo do número secreto!');
