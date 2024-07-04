295072941a611c41f20e6a0f06828c3f7b10e2e8
Igor Sopes <sopesigor@gmail.com>
2024-07-04 14:26:04 -0300

    adicionado while

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 alert('Bem vindo ao jogo do número secreto!');
 let numeroSecreto = 4;
 console.log(numeroSecreto)
-let chute = prompt('Escolha um número entre 1 e 10');
+let chute 
+while (chute!= numeroSecreto)
+    chute= prompt('Escolha um número entre 1 e 10');
 
 if (chute == numeroSecreto) {
     alert(`Parabéns, você acertou o número secreto! O numero secreto é ${numeroSecreto}`);
