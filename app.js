e368a478db46e93fc1e5b39053b13af3823b2d8e
Igor Sopes <sopesigor@gmail.com>
2024-07-03 23:58:49 -0300

    novo commit

--- app.js
+++ app.js
@@ -6,10 +6,11 @@ let chute = prompt('Escolha um número entre 1 e 10');
 if (chute == numeroSecreto) {
     alert(`Parabéns, você acertou o número secreto! O numero secreto é ${numeroSecreto}`);
 } else {
-    alert ('Você errou! Tente de novo')
-
-} else {
     if (numeroSecreto > chute) {
         alert(`O número secreto é maior que ${chute}`);
+    } else {
+        alert(`O número secreto é menor que ${chute}`);
+    
+    }
 
 }
