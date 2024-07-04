c21816a6470de86f421a7041e6040d39586e78b5
Igor Sopes <sopesigor@gmail.com>
2024-07-04 14:32:53 -0300

    novos operadores adicionados

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 alert('Bem vindo ao jogo do número secreto!');
 let numeroSecreto = 4;
-console.log(numeroSecreto)
+console.log(numeroSecreto);
 let chute 
 while (chute!= numeroSecreto)
     chute= prompt('Escolha um número entre 1 e 10');
 
-if (chute == numeroSecreto) {
+{if (chute == numeroSecreto) {
     alert(`Parabéns, você acertou o número secreto! O numero secreto é ${numeroSecreto}`);
 } else {
     if (numeroSecreto > chute) {
@@ -13,6 +13,6 @@ if (chute == numeroSecreto) {
     } else {
         alert(`O número secreto é menor que ${chute}`);
     
-    }
+    }}
 
 }
