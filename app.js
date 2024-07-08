3b39ce6722e2a9d491c888514e3fc267eb8870e4
Igor Sopes <sopesigor@gmail.com>
2024-07-08 15:39:34 -0300

    novas logicas

--- app.js
+++ app.js
@@ -4,17 +4,27 @@ console.log(numeroSecreto);
 let chute;
 let tentativas = 1;
  
-while (chute!= numeroSecreto)
+while (chute!= numeroSecreto) 
     chute= prompt('Escolha um número entre 1 e 10');
 
-    {if (chute == numeroSecreto) {
-    alert(`Parabéns, você acertou o número secreto! O numero secreto é ${numeroSecreto} com ${tentativas} tentativas` );
+    if (chute == numeroSecreto) {
+        break;
     } else {
     if (numeroSecreto > chute) {
         alert(`O número secreto é maior que ${chute}`);
     } else {
         alert(`O número secreto é menor que ${chute}`);
     
-    }}
+    }
     tentativas++;
-}
+} 
+
+let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa'
+alert (`Isso aí você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}`);
+
+// if (tentativas > 1) {
+
+     // alert(`O número secreto era ${numeroSecreto} e você acertou com apenas ${tentativas} tentativas`);
+    //} else {
+      // alert(`O número secreto era ${numeroSecreto} e você acertou com apenas ${tentativas} tentativa`);
+// }
