317f2925a8c12854539b77dcab40020e54d05ff9
Igor Sopes <sopesigor@gmail.com>
2024-07-17 18:30:39 -0300

    bug resolvido

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
-alert('Bem vindo ao jogo do número secreto!');
-let numeroSecreto = 4;
+alert('Boas vindas ao jogo do número secreto!');
+let numeroSecreto = parseInt(Math.random() * 100 +1 );
 console.log(numeroSecreto);
 let chute;
 let tentativas = 1;
  
-while (chute!= numeroSecreto) 
-    chute= prompt('Escolha um número entre 1 e 10');
+// enquanto o chute não for igual ao número secreto, o loop continua
+while (chute!= numeroSecreto) {
+    chute = prompt(`Escolha um número entre 1 e 100`);
 
     if (chute == numeroSecreto) {
+        
         break;
+
     } else {
     if (numeroSecreto > chute) {
         alert(`O número secreto é maior que ${chute}`);
@@ -16,7 +19,8 @@ while (chute!= numeroSecreto)
         alert(`O número secreto é menor que ${chute}`);
     
     }
-    tentativas++;
+        tentativas++;
+    }
 } 
 
 let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa'
@@ -27,4 +31,4 @@ alert (`Isso aí você descobriu o número secreto ${numeroSecreto} com ${tentat
      // alert(`O número secreto era ${numeroSecreto} e você acertou com apenas ${tentativas} tentativas`);
     //} else {
       // alert(`O número secreto era ${numeroSecreto} e você acertou com apenas ${tentativas} tentativa`);
-// }
+
